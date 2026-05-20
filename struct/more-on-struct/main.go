ceb461861acbd60e70454093f69d81fdbd8d8ff2
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-05-20 22:43:59 +0600

    struct

---
+++ struct/more-on-struct/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+// type user struct {
+// 	name       string
+// 	age        int
+// 	isLoggedIn bool
+// 	greet      func()
+// }
+
+type user struct {
+	name       string
+	age        int
+	isLoggedIn bool
+}
+
+func main() {
+	fmt.Println("more func on struct")
+	// user1 := user{
+
+	// 	name:       "sujon",
+	// 	age:        20,
+	// 	isLoggedIn: false,
+	// 	greet: func() {
+	// 		fmt.Println("hello there")
+	// 	},
+	// }
+
+	// user1.greet = func() {
+	// 	fmt.Println("hello", user1.name)
+	// }
+
+	// user1.greet()
+
+	user1 := user{
+		name:       "sujon",
+		age:        20,
+		isLoggedIn: false,
+	}
+	user1.greet()
+}
+
+func (u user) greet() {
+	fmt.Println("hello", u.name)
+
+}
