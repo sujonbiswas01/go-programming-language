fb88208b45f53a39c3f5e174a533e42373bc2aca
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-05-25 22:49:41 +0600

    clouser

--- 67-70/closure/main.go
+++ 67-70/closure/main.go
@@ -1,7 +1,32 @@
-package closure
+package main
 
 import "fmt"
 
+func multiplyBy(factor int) func(int) int {
+	return func(x int) int {
+		return x * factor
+
+	}
+}
+
+func counter() func() int {
+	count := 0
+	return func() int {
+		count++
+		return count
+	}
+}
+
 func main() {
-	fmt.Println("closer")
+	fmt.Println("closure")
+
+	// double := multiplyBy(2)
+	// fmt.Println(double(55), "bd")
+
+	increment := counter()
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(increment())
+	}
+
 }
