5b8100d77ef99709ca294842f58bdbe606dc9ee7
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-09-07 06:02:45 +0600

    post

--- WebsiteHealthCheckher/go-http-curd/createmethod.go
+++ WebsiteHealthCheckher/go-http-curd/createmethod.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,10 +12,23 @@ type Users struct {
 }
 
 func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Println("only post method is allow")
-		return
+	// if r.Method != "POST" {
+	// 	w.WriteHeader(http.StatusMethodNotAllowed)
+	// 	fmt.Println("only post method is allow")
+	// 	return
+	// }
+
+	var newUser User
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println("user post failed")
 	}
+	newUser.ID = len(Datas) + 1
+	users := append(Datas, newUser)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newUser)
 
 }
--- WebsiteHealthCheckher/go-http-curd/main.go
+++ WebsiteHealthCheckher/go-http-curd/main.go
@@ -8,7 +8,31 @@ import (
 )
 
 type User struct {
-	name string
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
+}
+
+var Datas = []User{
+	{
+		ID:    1,
+		Name:  "sujon biswas",
+		Age:   28,
+		Email: "sujon@example.com",
+	},
+	{
+		ID:    2,
+		Name:  "rajon biswas",
+		Age:   25,
+		Email: "rajon@example.com",
+	},
+	{
+		ID:    3,
+		Name:  "madob",
+		Age:   30,
+		Email: "madob@example.com",
+	},
 }
 
 func main() {
@@ -16,6 +40,7 @@ func main() {
 	mux.HandleFunc("/", rootHandler)
 	mux.HandleFunc("/health", healthHandler)
 	mux.HandleFunc("POST /createuser", CreateUserHandler)
+	mux.HandleFunc("GET /users", GgetUserhandler)
 	// http.HandleFunc("/", rootHandler)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 
@@ -34,10 +59,18 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	data := map[string]User{
 		"su": {
-			name: "sujon biswas",
+			Name: "sujon biswas",
 		},
 	}
 	jsondata, _ := json.Marshal(data)
 	w.Header().Set("Content-type", "application/json")
 	fmt.Fprintln(w, string(jsondata))
+}
+
+func GgetUserhandler(w http.ResponseWriter, r *http.Request) {
+	// userss, _ := json.Marshal(datas)
+	// w.Write(userss)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(Datas)
 }
